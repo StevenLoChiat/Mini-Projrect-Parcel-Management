e1c2101a9ff6d6d8bddd01b8a4fb5f6f7d07dbd7
MuhammadAzfar1313 <muhammadazfar13@student.usm.my>
2026-01-12 18:19:07 +0800

    Progress

--- saveData.cpp
+++ saveData.cpp
@@ -6,5 +6,19 @@ using namespace std;
 
 void saveData(const vector<Parcel>& parcels)
 {
+    int exit;
+    cout << "Are you sure you want to exit the programme?(Y/N) :";
+    cin >> exit;
+    if (exit == 'Y')
+    {
 
+    }
+    if (exit == 'N')
+    {
+        showMenu();
+    }
+    else
+    {
+        cout << "Invalid input";
+    }
 }
