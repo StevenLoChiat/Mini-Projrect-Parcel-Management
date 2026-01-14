cebd14e7ccd92b19bf5a241a905449663c4f2288
mingyang-usm <mingyang@student.usm.my>
2026-01-14 13:43:01 +0800

    finished

--- editParcel.cpp
+++ editParcel.cpp
@@ -1,10 +1,38 @@
-//MUHAMMAD AZFAR BIN ZULKIFLI
 #include <iostream>
 #include "header.hpp"
 
 using namespace std;
 
-void editParcel(vector<Parcel>& parcels)
-{
+void editParcel(vector<Parcel>& parcels) {
+    if (parcels.empty()) {
+        cout << "\n[!] No parcels available to edit.\n";
+        return;
+    }
 
+    string targetID;
+    cout << "Enter Parcel ID to edit: ";
+    getline(cin, targetID);
+
+    for (auto& p : parcels) {
+        if (p.id == targetID) {
+
+            cout << "Current Receiver Name: " << p.receiver << endl;
+            cout << "Enter new Receiver Name: ";
+            getline(cin, p.receiver);
+
+            cout << "Current Status: " << p.status << endl;
+            cout << "Enter new Status (Pending / Delivered / In Transit): ";
+            getline(cin, p.status);
+
+            if (!isValidStatus(p.status)) {
+                cout << "Invalid status entered! Edit cancelled." << endl;
+                return;
+            }
+
+            cout << "Parcel information updated successfully!" << endl;
+            return;
+        }
+    }
+
+    cout << "Parcel ID not found!" << endl;
 }
